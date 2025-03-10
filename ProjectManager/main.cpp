7261adc879e84b2659f42077e0fcd765cbfc9c4f
unknown <rotemk2008@ofekschool.org.il>
2025-03-10 21:04:42 +0200

    fix main

--- ProjectManager/main.cpp
+++ ProjectManager/main.cpp
@@ -9,4 +9,12 @@ int main()
         << "delete <project-name>\n"
         << "modify <project-name> <new-name>"
         << std::endl;
+
+    std::cout << "Projects Manager - v0.0.1\n"
+        << "-------------------------\n"
+        << "new <project-name> <author>\n"
+        << "show <?project-name>\n"
+        << "delete <project-name>\n"
+        << "modify <project-name> <new-name>"
+        << std::endl;
 }
